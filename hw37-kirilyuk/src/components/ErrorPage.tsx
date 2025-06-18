df6bb7882ac6c31e14492df9a9ab3ad83e33514c
Dmitriy <kirilyuk.dm@gmail.com>
2025-06-18 14:51:38 +0300

    hw37 v1

--- hw37-kirilyuk/src/components/ErrorPage.tsx
+++ hw37-kirilyuk/src/components/ErrorPage.tsx
@@ -37,7 +37,7 @@ const ErrorPage = () => {
             justifyContent: 'center',
             alignItems: 'center'
         }}>
-            <h1>404 - Страница не найдена</h1>
+            <h1>404 - Страница не найдена!!!</h1>
             <p>Извините, запрашиваемая страница не существует.</p>
             <button
                 onClick={handleGoHome}
